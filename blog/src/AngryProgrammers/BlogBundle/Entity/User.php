7c89f5ce1148dcba93a104f2da423634d54636e9
SirGladius <brisset.fabien@hotmail.fr>
2015-11-03 22:50:23 +0100

    Mise en place de FOSUserBundle mais non utilisé pour le moment

---
+++ blog/src/AngryProgrammers/BlogBundle/Entity/User.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AngryProgrammers\BlogBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="fos_user")
+ * @ORM\Entity(repositoryClass="AngryProgrammers\BlogBundle\Entity\UserRepository")
+ */
+class User extends BaseUser
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
+
